da88a32d2dd38542f845b9e21b78ae9b4036be53
Brenda <basl1@aluno.ifal.edu.br>
2026-07-19 16:29:53 -0300

    1.1

--- advinha.js
+++ advinha.js
@@ -1,25 +1,23 @@
-const numero = document.getElementById ('numeroN');
 const button = document.getElementById ('botao');
 const resultadoReal = Math.floor(Math.random()*50) + 1;
 
     const button = button.addEventListener('click', function(){
-        
-        const palpite = document.getElementById('palpite');
+
         let palpite;
 
         do {
-            palpite = parseInt(prompt("Tente adivinhar o número secreto entre 1 e 50:"));
+            numero = parseInt(prompt("Tente adivinhar o número secreto entre 1 e 50:"));
 
 
-            if(palpite > resultadoReal){
-                alert("Ah, que pena, o número secreto é maior que esse");
-                
-            }else (palpite < resultadoReal){
+            if(numero > resultadoReal){
                 alert("Ah, que pena, o número secreto é menor que esse");
+                
+            }else if (numero < resultadoReal){
+                alert("Ah, que pena, o número secreto é maior que esse");
                 
             }
 
-        } while (resultadoReal != palpite);
+        } while (resultadoReal != numero);
 
         alert("Parábens, você acertou! E o numero secreto é ("+resultadoReal+")!");
 
